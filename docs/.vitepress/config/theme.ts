07808a86a89dda74d22cb58868b7a79f910e3503
Yanjin Li <yanjinli502@gmail.com>
2024-09-07 18:04:12 +0800

    add local search

--- docs/.vitepress/config/theme.ts
+++ docs/.vitepress/config/theme.ts
@@ -1,11 +1,51 @@
 import type { DefaultTheme } from "vitepress/theme-without-fonts";
-import algolia from "./algolia";
+// import algolia from "./algolia";
 import { LOGO as logo, REPO } from "./info";
 import nav from "./nav";
 import sidebar from "./sidebar";
 
 const themeConfig: DefaultTheme.Config = {
-  algolia,
+  // algolia,
+  search: {
+    provider: "local",
+    options: {
+      locales: {
+        root: {
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              displayDetails: "显示详细列表",
+              resetButtonTitle: "重置搜索",
+              backButtonTitle: "关闭搜索",
+              noResultsText: "没有结果",
+              footer: {
+                selectText: "选择",
+                selectKeyAriaLabel: "输入",
+                navigateText: "导航",
+                navigateUpKeyAriaLabel: "上箭头",
+                navigateDownKeyAriaLabel: "下箭头",
+                closeText: "关闭",
+                closeKeyAriaLabel: "esc",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  darkModeSwitchLabel: "颜色选择",
+  lightModeSwitchTitle: "切换至亮色模式",
+  darkModeSwitchTitle: "切换至暗色模式",
+  sidebarMenuLabel: "目录",
+  returnToTopLabel: "回到顶部",
+  externalLinkIcon: true,
+  outline: {
+    label: "目录",
+    level: [2, 6],
+  },
   editLink: {
     pattern: `${REPO}/edit/main/docs/:path`,
     text: "在 GitHub 上编辑此页",
@@ -15,11 +55,15 @@ const themeConfig: DefaultTheme.Config = {
       'Availably free under <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh" class="grabient-text">CC BY-SA 4.0</a>.',
     copyright: `Made with ❤️️ by <a href="${REPO}/graphs/contributors" class="grabient-text">all contributors</a>.`,
   },
+  docFooter: {
+    prev: "上一节",
+    next: "下一节",
+  },
   lastUpdatedText: "📑 最后更新",
   logo,
   nav,
   sidebar,
-  siteTitle: "macOS不完全指南",
+  siteTitle: "VS 不完全指南",
   socialLinks: [{ icon: "github", link: REPO }],
 };
 
